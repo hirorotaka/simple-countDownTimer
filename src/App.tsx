61f0fb8b543623fd02bcfa09c400246c0f807996
hiroki takada <hirorotaka5@gmail.com>
2024-06-27 13:26:12 +0900

    ディスプレイの表示を作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 import { TimeSetter } from './components/TimeSetter';
 import { TimerControls } from './components/TimerControls';
@@ -5,9 +6,14 @@ import { TimerDisplay } from './components/TimerDisplay';
 import { TimerStatus } from './components/TimerStatus';
 
 function App() {
+  const [displayTimer, setDisplayTimer] = useState({
+    hour: 1,
+    minute: 2,
+    second: 3,
+  });
   return (
     <div className="container mx-auto px-4 py-8">
-      <TimerDisplay />
+      <TimerDisplay displayTimer={displayTimer} />
       <TimeSetter />
       <TimerControls />
       <TimerStatus />
--- src/components/TimerDisplay.tsx
+++ src/components/TimerDisplay.tsx
@@ -1,12 +1,28 @@
+interface TimerDisplayProps {
+  displayTimer: {
+    hour: number;
+    minute: number;
+    second: number;
+  };
+}
+
 //タイマー表示
-export const TimerDisplay = () => {
+export const TimerDisplay = ({ displayTimer }: TimerDisplayProps) => {
   return (
     <div className="text-center">
-      <h1 className="mb-8 text-4xl font-bold">カウントダウンタイマー</h1>
-      <div className="mb-2">
-        <span>開始:</span> <span className="text-right">--:--:--</span>
+      <div className="mb-8 flex justify-center">
+        <div className="inline-flex rounded-lg bg-gray-200 p-4">
+          <div
+            className="font-mono text-6xl font-bold tabular-nums tracking-tight"
+            style={{ minWidth: '240px' }}
+          >
+            {/* 文字列が２桁になるように前に0を追加 */}
+            {String(displayTimer.hour).padStart(2, '0')}:
+            {String(displayTimer.minute).padStart(2, '0')}:
+            {String(displayTimer.second).padStart(2, '0')}
+          </div>
+        </div>
       </div>
-      <div className=" mb-8 text-6xl font-bold">00:00:00</div>
     </div>
   );
 };
